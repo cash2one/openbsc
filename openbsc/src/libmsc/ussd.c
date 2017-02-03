24bb905f7be77efe94e8d9eb4a9a89f14271b22e
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2017-02-03 02:42:47 +0100

    Use libvlr in libmsc (large refactoring)
    Original libvlr code is by Harald Welte <laforge@gnumonks.org>,
    polished and tweaked by Neels Hofmeyr <nhofmeyr@sysmocom.de>.
    
    This is a long series of development collapsed in one patch. The original
    history may still be available as branch neels/vlr_orig.
    
    TODO: This commit may be split in several smaller changes before merging to
    master.
    
    SMS:
    
    The SQL based lookup of SMS for attached subscribers no longer works since the
    SQL database no longer has the subscriber data. Replace with a round-robin on
    the SMS recipient MSISDNs paired with a VLR subscriber RAM lookup whether the
    subscriber is currently attached.
    
    If there are many SMS for not-attached subscribers in the SMS database, this
    will become inefficient: a DB hit returns a pending SMS, the RAM lookup will
    reveal that the subscriber is not attached, after which the DB is hit for the
    next SMS. It would become more efficient e.g. by having an MSISDN based hash
    list for the VLR subscribers and by marking non-attached SMS recipients in the
    SMS database so that they can be excluded with the SQL query already.
    
    There is a sanity limit to do at most 100 db hits per attempt to find a pending
    SMS. So if there are more than 100 stored SMS waiting for their recipients to
    actually attach to the MSC, it may take more than one SMS queue trigger to
    deliver SMS for subscribers that are actually attached.
    
    This is not very beautiful, but is merely intended to carry us over to a time
    when we have a proper separate SMSC entity.
    
    Related: OS#1592
    
    Change-Id: I702ba504ce2de93507312c28eca8d11f09f4ee8b

--- openbsc/src/libmsc/ussd.c
+++ openbsc/src/libmsc/ussd.c
@@ -33,6 +33,7 @@
 #include <openbsc/gsm_subscriber.h>
 #include <openbsc/debug.h>
 #include <openbsc/osmo_msc.h>
+#include <openbsc/vlr.h>
 
 /* Declarations of USSD strings to be recognised */
 const char USSD_TEXT_OWN_NUMBER[] = "*#100#";
@@ -52,6 +53,8 @@ int handle_rcv_ussd(struct gsm_subscriber_connection *conn, struct msgb *msg)
 	 * to keep the connection alive due ot ongoing USSD exchange.
 	 * As we answer everytying synchronously so far, there's no need
 	 * yet */
+
+	cm_service_request_concludes(conn, msg);
 
 	memset(&req, 0, sizeof(req));
 	gh = msgb_l3(msg);
@@ -73,6 +76,7 @@ int handle_rcv_ussd(struct gsm_subscriber_connection *conn, struct msgb *msg)
 		return 0;
 	}
 
+	msc_subscr_conn_communicating(conn);
 	if (!strcmp(USSD_TEXT_OWN_NUMBER, (const char *)req.ussd_text)) {
 		DEBUGP(DMM, "USSD: Own number requested\n");
 		rc = send_own_number(conn, msg, &req);
@@ -87,8 +91,11 @@ int handle_rcv_ussd(struct gsm_subscriber_connection *conn, struct msgb *msg)
 /* A network-specific handler function */
 static int send_own_number(struct gsm_subscriber_connection *conn, const struct msgb *msg, const struct ss_request *req)
 {
-	char *own_number = conn->subscr->extension;
+	char *own_number = conn->vsub->msisdn;
 	char response_string[GSM_EXTENSION_LENGTH + 20];
+
+	DEBUGP(DMM, "%s: MSISDN = %s\n", vlr_subscr_name(conn->vsub),
+	       own_number);
 
 	/* Need trailing CR as EOT character */
 	snprintf(response_string, sizeof(response_string), "Your extension is %s\r", own_number);
